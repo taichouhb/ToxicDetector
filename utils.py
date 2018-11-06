c6032689cb015b44ffc46f41a5f83820cdca0b63
Ben <b.cheung2@gmail.com>
2018-11-06 15:30:36 -0500

    Reverted the parser back before other revertion. Currently: noswear, disability, racial slurs are supported in featurization

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import pdb
 from collections import defaultdict
+import numpy as np
+
+
 
 def featurize(input):
     #Takes in Nx (2) array of tuples. 
     
     # Parse input sentence into D features for each sentence  
-    data = feats(input)
+    x, y = feats(input)
     
     
     #Perhaps need to take mini-batches, or we can train on the whole thing
@@ -14,26 +17,37 @@ def featurize(input):
     
     #Return data_train, data_val, data_test subsets of full data, 
     #data_train being largest, probably  
-    return None, None, None
+    return x, y
 
 def feats(input):
     #Parse input into features of length D.
     #Pandas DF N x 3 
-    globalTable = parseGlobal()
-    pdb.set_trace()
-    for n, row in input.iterrows():
-        idd, comment, index = row
+    globalTable, totalFeatures = parseGlobal()
+    featureMatrix = []
+    labels = []
+
+    for ind, row in input.iterrows():
+        featureVector = np.array([0]*totalFeatures)
+        idd, comment = row['id'], row['comment_text']
+        label = [row['toxic'], row['severe_toxic'], row['obscene'], \
+                 row['threat'], row['insult'], row['identity_hate']]
         #Tokenize comment
         newSet = ',.!?\"'
         exclamation = comment.count("!")
         table =  str.maketrans({key:None for key in newSet})
-        translated = comment.translate(table)
+        translated = comment.lower().translate(table)
         tokens = translated.split(" ")
+        for t in tokens: 
+            if t in globalTable:
+                for ind in globalTable[t]:
+                    featureVector[ind] += 1 
+        featureMatrix.append(featureVector)
+        labels.append(label)
         
-    
-    #output returns in N x D, N (labels, N of them)
-    #Perhaps preprocess the labels here so they are binary class, instead of 10 classes
-    return None, None
+    featureMatrix = np.array(featureMatrix)
+    labels = np.array(labels)
+    return featureMatrix, labels
+        
 
 def parseGlobal():
     directory = 'slurs/'
@@ -48,8 +62,7 @@ def parseGlobal():
             if w not in wordToFeature:
                 wordToFeature[w] = set()
             wordToFeature[w].add(idx)
-    return wordToFeature
-
+    return wordToFeature, max(fileNames, key=lambda x:x[1])[1]+1
 
 
 """
